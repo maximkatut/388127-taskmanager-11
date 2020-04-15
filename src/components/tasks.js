3bb3205c57d694a6e17ac775d79b7f7f3b39ca7d
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-04-15 17:52:53 -0400

    refactoring of board

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,22 +1,26 @@
 import {createElement} from "../utils.js";
 
-const createBoardTemplate = () => {
-  return `<section class="board container"></section>`;
+const createTasksTemplate = () => {
+  return (
+    `<div class="board__tasks"></div>`
+  );
 };
 
-export default class Board {
+
+export default class Tasks {
   constructor() {
     this._element = null;
   }
 
-  getTamplate() {
-    return createBoardTemplate();
+  getTemplate() {
+    return createTasksTemplate();
   }
 
   getElement() {
     if (!this._element) {
-      this._element = createElement(this.getTamplate());
+      this._element = createElement(this.getTemplate());
     }
+
     return this._element;
   }
 
